57c08fbc44307618601cae86e808c143766adb05
Laura Rodriguez <laura.rodriguez@okta.com>
2023-07-28 16:55:15 -0300

    Add IDP tests.

--- tests/Api/OktaIdentityProviderApi.Tests.ps1
+++ tests/Api/OktaIdentityProviderApi.Tests.ps1
@@ -24,10 +24,62 @@ Describe -tag 'Okta.PowerShell' -name 'OktaOktaIdentityProviderApi' {
     }
 
     Context 'New-OktaIdentityProvider' {
-        It 'Test New-OktaIdentityProvider' {
-            #$TestResult = New-OktaIdentityProvider -IdentityProvider "TEST_VALUE"
-            #$TestResult | Should -BeOfType TODO
-            #$TestResult.property | Should -Be 0
+        It 'Test Initialize-OktaIdentityProvider Facebook' {
+            
+            $IdpCredentialsClient = Initialize-OktaIdentityProviderCredentialsClient -ClientId "your-client-id" -ClientSecret "your-client-secret"
+            $IdpCredentials = Initialize-OktaIdentityProviderCredentials -Client $IdpCredentialsClient
+            $Protocol = Initialize-OktaProtocol -Credentials $IdpCredentials -Type 'OAUTH2' -Algorithms @("public_profile", "email" ) 
+            $AccountLink = Initialize-OktaPolicyAccountLink -Action 'AUTO'
+            $Conditions = Initialize-OktaProvisioningConditions -Deprovisioned @{Action = "NONE"} -Suspended @{Action = "NONE"} 
+            $Provisioning = Initialize-OktaProvisioning -Action 'AUTO' -ProfileMaster $True -Conditions $Conditions -Groups @{Action = "NONE"}
+            $Subject = Initialize-OktaPolicySubject -UserNameTemplate @{Template = "idpuser.email"} -MatchType "USERNAME" 
+            $Policy = Initialize-OktaIdentityProviderPolicy -AccountLink $AccountLink -Provisioning $Provisioning -Subject $Subject
+            $NewIdp = Initialize-OktaIdentityProvider -Name "New idp" -Type "FACEBOOK" -Protocol $Protocol -Policy $Policy
+
+            $NewIdp.Name | Should -Be "New idp"
+            $NewIdp.Type | Should -Be "FACEBOOK"
+            $NewIdp.Protocol.Credentials.Client.Client_Id | Should -Be "your-client-id"
+            $NewIdp.Protocol.Credentials.Client.Client_Secret | Should -Be "your-client-secret"
+            $NewIdp.Protocol.Type | Should -Be "OAUTH2"
+            $NewIdp.Protocol.Algorithms.Count | Should -Be 2
+            $NewIdp.Policy.AccountLink.Action | Should -Be "AUTO"
+            $NewIdp.Policy.Provisioning.Action | Should -Be "AUTO"
+            $NewIdp.Policy.Provisioning.ProfileMaster | Should -Be $True
+            $NewIdp.Policy.Provisioning.Conditions.Deprovisioned.Action | Should -Be "NONE"
+            $NewIdp.Policy.Provisioning.Conditions.Suspended.Action | Should -Be "NONE"
+            $NewIdp.Policy.Provisioning.Groups.Action | Should -Be "NONE"
+            $NewIdp.Policy.Subject.UserNameTemplate.Template  | Should -Be "idpuser.email"
+            $NewIdp.Policy.Subject.MatchType  | Should -Be "USERNAME"
+        }
+
+        It 'Test New-OktaIdentityProvider Facebook' {
+            $Content = '{"id":"0oa96k3igxBoeI0Rh1d7","issuerMode":"DYNAMIC","name":"New idp","status":"ACTIVE","created":"2023-07-28T19:52:19.000Z","lastUpdated":"2023-07-28T19:52:19.000Z","protocol":{"type":"OAUTH2","endpoints":{"authorization":{"url":"https://www.facebook.com/dialog/oauth","binding":"HTTP-REDIRECT"},"token":{"url":"https://graph.facebook.com/v2.8/oauth/access_token","binding":"HTTP-POST"}},"scopes":["public_profile","email"],"credentials":{"client":{"client_id":"your-client-id","client_secret":"your-client-secret"}}},"policy":{"provisioning":{"action":"AUTO","profileMaster":true,"groups":{"action":"NONE"},"conditions":{"deprovisioned":{"action":"NONE"},"suspended":{"action":"NONE"}}},"accountLink":{"filter":null,"action":"AUTO"},"subject":{"userNameTemplate":{"template":"idpuser.email"},"filter":null,"matchType":"USERNAME","matchAttribute":null},"maxClockSkew":0},"type":"FACEBOOK","_links":{"authorize":{"href":"https://testorg.com/oauth2/v1/authorize?idp=0oa96k3igxBoeI0Rh1d7&client_id={clientId}&response_type={responseType}&response_mode={responseMode}&scope={scopes}&redirect_uri={redirectUri}&state={state}&nonce={nonce}","templated":true,"hints":{"allow":["GET"]}},"clientRedirectUri":{"href":"https://testorg.com/oauth2/v1/authorize/callback","hints":{"allow":["POST"]}}}}' | ConvertFrom-Json
+
+            $Response = @{
+                Response   = $Content
+                StatusCode = 200
+                Headers = @{ "Content-Type" = @("application/json")}
+            }
+
+            Mock -ModuleName Okta.PowerShell Invoke-OktaApiClient { return $Response } -Verifiable
+
+            $NewIdp = New-OktaIdentityProvider -IdentityProvider @{}
+
+            Assert-MockCalled -ModuleName Okta.PowerShell Invoke-OktaApiClient -Times 1
+
+            $NewIdp.Name | Should -Be "New idp"
+            $NewIdp.Type | Should -Be "FACEBOOK"
+            $NewIdp.Protocol.Credentials.Client.Client_Id | Should -Be "your-client-id"
+            $NewIdp.Protocol.Credentials.Client.Client_Secret | Should -Be "your-client-secret"
+            $NewIdp.Protocol.Type | Should -Be "OAUTH2"
+            $NewIdp.Policy.AccountLink.Action | Should -Be "AUTO"
+            $NewIdp.Policy.Provisioning.Action | Should -Be "AUTO"
+            $NewIdp.Policy.Provisioning.ProfileMaster | Should -Be $True
+            $NewIdp.Policy.Provisioning.Conditions.Deprovisioned.Action | Should -Be "NONE"
+            $NewIdp.Policy.Provisioning.Conditions.Suspended.Action | Should -Be "NONE"
+            $NewIdp.Policy.Provisioning.Groups.Action | Should -Be "NONE"
+            $NewIdp.Policy.Subject.UserNameTemplate.Template  | Should -Be "idpuser.email"
+            $NewIdp.Policy.Subject.MatchType  | Should -Be "USERNAME"
         }
     }
 
