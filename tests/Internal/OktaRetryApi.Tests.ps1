aab1221977d85a2c2e4296bc39be7d00a9e95a9d
Laura Rodriguez <rdz.maria.laura@gmail.com>
2025-02-03 16:02:46 -0500

    - Update AddRetryHeaders so it doesn't return any objects that might affect the pipeline - Add unit tests that verify that IWR doesn't throw with 4xx responses so retry logic can be executed if it's applicable - Drop support for PS 6.x version
    Signed-off-by: Laura Rodriguez <rdz.maria.laura@gmail.com>

--- tests/Internal/OktaRetryApi.Tests.ps1
+++ tests/Internal/OktaRetryApi.Tests.ps1
@@ -1,6 +1,5 @@
 Describe -tag 'Okta.PowerShell' -name 'OktaRetryApi' {
-    Context 'ShouldRetry' {
-
+    Context 'ShouldRetry tests' {
         It 'Should retry 429 responses when retryCount < MaxRetries' {
             
             $Config = Get-OktaConfiguration
@@ -171,6 +170,30 @@ Describe -tag 'Okta.PowerShell' -name 'OktaRetryApi' {
 
             Assert-MockCalled -ModuleName Okta.PowerShell Get-OktaConfiguration -Times 1
         }
+
+        It 'Should get rate-limit-params regardless of the casing' {
+            
+            $Config = @{
+                MaxRetries   = 2
+                RequestTimeout = 2000
+            }
+
+            $Now = Get-Date # Used as a reference for the test. Indicates when the request was executed
+            $ResetDate = $Now.AddSeconds(3) # Indicates when one should retry
+            $ResetDateEpoch = $ResetDate.ToUniversalTime().Subtract((New-Object DateTime(1970, 1, 1, 0, 0, 0, 0))).TotalSeconds
+
+            Mock -ModuleName Okta.PowerShell Get-OktaConfiguration { return $Config } -Verifiable
+
+            $Headers1 = @{"X-rAte-Limit-RESET" = @($ResetDateEpoch); "DATE" = @($Now)}
+
+            # $BackoffInMilliseconds = (New-TimeSpan -Start $RequestUtcDate -End $RetryAtUtcTime).Milliseconds + 1000 #delta
+            $Result = CalculateDelay -Headers $Headers
+
+            Assert-MockCalled -ModuleName Okta.PowerShell Get-OktaConfiguration -Times 1
+
+            [int]$intResult = $result
+            $intResult | Should -Be 4000
+        }
     }
 
     Context 'AddRetryHeaders' {
@@ -180,10 +203,80 @@ Describe -tag 'Okta.PowerShell' -name 'OktaRetryApi' {
             # $Headers["X-Okta-Retry-For"] = $RequestId
             # $Headers["X-Okta-Retry-Count"] = $RetryCount
 
-            $Result = AddRetryHeaders -Headers $Headers -RequestId "foo" -RetryCount 5
+            AddRetryHeaders -Headers $Headers -RequestId "foo" -RetryCount 5
 
-            $Result["X-Okta-Retry-For"] | Should -Be "foo"
-            $Result["X-Okta-Retry-Count"] | Should -Be 5
+            $Headers["X-Okta-Retry-For"] | Should -Be "foo"
+            $Headers["X-Okta-Retry-Count"] | Should -Be 5
+        }
+    }
+
+    Context 'Retry tests using HtppListener'{
+        BeforeAll {
+            Import-Module -Name "$PSScriptRoot\HttpListener\HttpListener.psm1" -Verbose 
+        }
+    
+        AfterAll {
+            Remove-Module -Name HttpListener -Verbose
+        }
+
+        # This test is mainly to verify that we can test the internal IWR call used by OktaApiClient using the HttpListener instead of Pester.
+        # Using Pester doesn't allow us to test some specifics of IWR, such as using the -SkipHttpErrorCheck flag 
+        It '429 responses should not throw when -SkipHttpCheckError is included (2.x series)' {
+            $Now = Get-Date # Used as a reference for the test. Indicates when the request was executed
+            $ResetDate = $Now.AddSeconds(3) # Indicates when one should retry
+            $port = 9000
+            $headers = @{'X-Okta-Request-Id'='foo';'X-Rate-Limit-Reset'=$ResetDate;'Date'=$Now;'test'='bar'} | ConvertTo-Json -Compress 
+            $Uri = "http://127.0.0.1:$port/okta-powershell/?test=response&headers=$headers&statuscode=429"
+
+            $Config = Get-OktaConfiguration
+            $Config.MaxRetries = 0
+            $Config.RequestTimeout = $null
+            $Config.BaseUrl = "http://127.0.0.1:$port"
+            
+            Mock -ModuleName Okta.PowerShell Get-OktaConfiguration { return $Config } -Verifiable
+
+            $LocalVarAccepts = @('application/json')
+            $LocalVarContentTypes = @()
+            $LocalVarQueryParameters = @{}
+            $LocalVarHeaderParameters = @{}
+            $LocalVarFormParameters = @{}
+            $LocalVarCookieParameters = @{}
+            $LocalVarBodyParameter = $null
+            $StatusCodeFromException = $null
+
+            try {
+                
+                $ParsedUri = Invoke-ParseAbsoluteUri -Uri $Uri
+                $LocalVarUri = $ParsedUri["RelativeUri"]
+                $LocalVarQueryParameters = $ParsedUri["QueryParameters"]
+
+                { Start-HTTPListener -Port $port -Verbose} | Should -Not -Throw
+
+                $Result = Invoke-OktaApiClient -Method 'GET' `
+                                        -Uri $LocalVarUri `
+                                        -Accepts $LocalVarAccepts `
+                                        -ContentTypes $LocalVarContentTypes `
+                                        -Body $LocalVarBodyParameter `
+                                        -HeaderParameters $LocalVarHeaderParameters `
+                                        -QueryParameters $LocalVarQueryParameters `
+                                        -FormParameters $LocalVarFormParameters `
+                                        -CookieParameters $LocalVarCookieParameters `
+                                        -ReturnType "Application[]" `
+                                        -IsBodyNullable $false 
+
+                $Result.StatusCode | Should -Be 429
+                $Result.Headers['X-Okta-Request-Id'] | Should -Be "foo"
+                $Result.Headers['X-Rate-Limit-Reset'] | Should -Be $ResetDate
+                $Result.Headers['Date'] | Should -Be $Now
+                $Result.Headers['test'] | Should -Be "bar"
+            }
+            catch{
+                $StatusCodeFromException = $_.Exception.Response.StatusCode.value__ 
+            }
+            finally {
+                Stop-HTTPListener -Port $port
+                $StatusCodeFromException | Should -Be $null
+            }
         }
     }
 }
